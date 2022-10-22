9cc7f3878d1b5c3829d6b27390df7eefd4f38aaf
Leonardo <leowitcroz@gmail.com>
2022-10-21 23:17:32 -0300

    fundo feito

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ ajustaTamanhoPalco();
 
 
 function posicaoRandomica() {
+
+    if(document.getElementById('mosca')){
+        document.getElementById('mosca').remove()
+    }
+
+
+
   var posicaoX = Math.floor(Math.random() * largura) - 90;
   var posicaoY = Math.floor(Math.random() * altura) - 90;
 
@@ -25,15 +32,49 @@ function posicaoRandomica() {
 
   ///////////////////
 
+
+
 var mosca = document.createElement('img')
 mosca.src = 'imagens/mosca.png'
-mosca.className ='mosquito-1'
+mosca.className = tamanhoAleatorio() + ladoAleatorio()
 
 mosca.style.left = posicaoX + 'px'
 mosca.style.top = posicaoY + 'px'
 mosca.style.position = 'absolute'
-
+mosca.id = 'mosca'
 
 document.body.appendChild(mosca)
+
+
+
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+
+    if(classe == 0){
+        return 'mosquito-1 '
+    }
+    else if(classe == 1){
+        return 'mosquito-2 '
+    }
+    else if(classe == 2){
+        return 'mosquito-3 '
+    }
+
+}
+
+function ladoAleatorio(){
+
+    var classe = Math.floor(Math.random() * 2)
+
+    if(classe == 0){
+        return 'lado-A'
+    }
+    else if(classe == 1){
+        return 'lado-B'
+    }
+   
+ 
 
 }
